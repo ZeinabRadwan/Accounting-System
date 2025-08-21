c39ad943bf6d49203e3450334f0cd214a8508aac
dani7999 <87643733+dani7999@users.noreply.github.com>
2025-08-21 15:20:44 +0500

    fixes

--- database/seeders/Demo/DemoUserRoleTableSeeder.php
+++ database/seeders/Demo/DemoUserRoleTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders\Demo;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DemoUserRoleTableSeeder extends Seeder
 {
@@ -13,29 +14,38 @@ class DemoUserRoleTableSeeder extends Seeder
      */
     public function run()
     {
-        \DB::table('user_role')->delete();
 
-        \DB::table('user_role')->insert([
-            0 => [
-                'user_id' => 1,
-                'role_id' => 1,
-            ],
-            1 => [
-                'user_id' => 2,
-                'role_id' => 2,
-            ],
-            2 => [
-                'user_id' => 3,
-                'role_id' => 3,
-            ],
-            3 => [
-                'user_id' => 4,
-                'role_id' => 2,
-            ],
-            4 => [
-                'user_id' => 5,
-                'role_id' => 1,
-            ],
-        ]);
+        if (DB::table('user_role')->count() == 0) {
+            DB::table('user_role')->insert([
+                [
+                    'user_id' => 1,
+                    'role_id' => 1,
+                ],
+            ]);
+        }
+        // \DB::table('user_role')->delete();
+
+        // \DB::table('user_role')->insert([
+        //     0 => [
+        //         'user_id' => 1,
+        //         'role_id' => 1,
+        //     ],
+        //     1 => [
+        //         'user_id' => 2,
+        //         'role_id' => 2,
+        //     ],
+        //     2 => [
+        //         'user_id' => 3,
+        //         'role_id' => 3,
+        //     ],
+        //     3 => [
+        //         'user_id' => 4,
+        //         'role_id' => 2,
+        //     ],
+        //     4 => [
+        //         'user_id' => 5,
+        //         'role_id' => 1,
+        //     ],
+        // ]);
     }
 }
