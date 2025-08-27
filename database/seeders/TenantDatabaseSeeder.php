3fef47e838e4118b1fcf1380c3f756f1ac91ca20
Abdelnabi M Ahmed <abdelnabi@yahoo.com>
2025-08-27 14:46:00 +0300

    fix register  on line

--- database/seeders/TenantDatabaseSeeder.php
+++ database/seeders/TenantDatabaseSeeder.php
@@ -18,6 +18,13 @@ class TenantDatabaseSeeder extends Seeder
         // for testing purposes
         // this will not run in production environment
         if (App::environment('local')) {
+            // First create chart of accounts to avoid foreign key constraint issues
+            $this->call([
+                TenantChartOfAccountType::class,
+                TenantChartOfAccountSeeder::class,
+            ]);
+            
+            // Then run demo seeders
             $this->call([
                 DemoDatabaseSeeder::class,
             ]);
