f2fecd0762b86e9260139ff293512c72b4dcfcf0
Zeinab Abdelghafar <zeinababdelghafar88@gmail.com>
2025-09-15 13:15:46 +0300

    fix the theme settings sidebar toggle issue in the navbar

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -119,7 +119,7 @@
         </a>
       </li>
       <li class="nav-item" v-tooltip="'Theme Settings'">
-        <a class="nav-link" @click="sideBarControl" href="#">
+        <a class="nav-link" @click.prevent="sideBarControl" href="#">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
             stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round"
--- resources/js/components/central/Navbar.vue
+++ resources/js/components/central/Navbar.vue
@@ -28,7 +28,7 @@
         </a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" @click="sideBarControl" href="#">
+        <a class="nav-link" @click.prevent="sideBarControl" href="#">
           <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"
             stroke-width="2">
             <path stroke-linecap="round" stroke-linejoin="round"
