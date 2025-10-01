c4c3d58077e6222b8c4708cdec2d5d8e4a0c0c5a
Mohamed Selem <35905302+mselem0@users.noreply.github.com>
2025-10-01 17:19:27 +0300

    fixes

--- resources/js/tenant.js
+++ resources/js/tenant.js
@@ -66,9 +66,20 @@ Vue.prototype.$axios = axios
 Vue.prototype.$http = axios
 
 /* eslint-disable no-new */
-new Vue({
+// Ensure router is properly initialized before creating Vue instance
+if (router) {
+  // Set a temporary app reference to prevent vue-meta errors
+  router.app = { $router: router };
+}
+
+const app = new Vue({
   i18n,
   store,
   router,
   ...App
 })
+
+// Update the router app reference with the actual Vue instance
+if (router) {
+  router.app = app;
+}
