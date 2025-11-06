5e60bf7e5d29551453ba78c7d83b058d50f9b08a
dani7999 <87643733+dani7999@users.noreply.github.com>
2025-11-06 18:24:38 +0500

    fixes

--- public/upg.php
+++ public/upg.php
@@ -50,8 +50,8 @@ class UpdateSettingCommand
         $command = 'git fetch --depth=2 && git reset --hard origin/main';
 
         // Set the working directory and status file
-        $work_folder = __DIR__ . '/../repositories/Arqam-System';
-        $status_file = __DIR__ . '/../repositories/Arqam-System/github_status.json';
+        $work_folder = __DIR__ . '/../repositories/Accounting-System';
+        $status_file = __DIR__ . '/../repositories/Accounting-System/github_status.json';
 
         // Change the working directory
         chdir($work_folder);
