7747c59c6696ff824e42430656807dbb581e1df4
dani7999 <87643733+dani7999@users.noreply.github.com>
2025-11-18 17:12:14 +0500

    fixes

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,7 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/print/reports/*',
         '/api/*',
         'stripe/*',
         '/updateSystem',
