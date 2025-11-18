a4a2d5a0e3f0dc2444bba31aadc1309f4b672d82
dani7999 <87643733+dani7999@users.noreply.github.com>
2025-11-18 15:02:26 +0500

    Update ReportController.php

--- app/Http/Controllers/API/ReportController.php
+++ app/Http/Controllers/API/ReportController.php
@@ -1761,7 +1761,7 @@ class ReportController extends Controller
             'itemName' => 'required',
         ]);
 
-        $user = Auth::user();
+        $user = auth('session')->user();
         dd($user);
         $branchIds = $this->getUserBranchIds($user);
         $allProducts = [];
