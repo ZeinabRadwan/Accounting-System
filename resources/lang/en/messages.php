1a7d4f1bf19d52a7978e124142391b0899b5b068
Zeinab Abdelghafar <zeinababdelghafar88@gmail.com>
2025-11-19 12:51:55 +0200

    Add localization for 'Filter by Operation Type' in English and Arabic

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -30,4 +30,7 @@ return [
     
     // Authentication Messages
     'The provided credentials are incorrect.' => 'The provided credentials are incorrect.',
+    
+    // Inventory / UI Messages
+    'Filter by Operation Type' => 'Filter by Operation Type',
 ];
